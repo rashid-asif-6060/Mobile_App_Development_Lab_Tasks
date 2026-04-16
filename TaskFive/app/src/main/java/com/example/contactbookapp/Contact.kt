8d8499290c9991495659e0edfa0ce4e6b6543b42
Rashid Asif <asif@Asifs-MacBook-Air.local>
2026-04-16 19:39:14 +0600

    Done task One

--- TaskFive/app/src/main/java/com/example/contactbookapp/Contact.kt
+++
@@ -1,8 +0,0 @@
-package com.example.contactbookapp
-
-data class Contact(
-    val name: String,
-    val phone: String,
-    val email: String,
-    val initial: String
-)
--- TaskSeven/app/src/main/java/com/example/photogalleryapp/Photo.kt
+++
@@ -1,9 +0,0 @@
-package com.example.photogalleryapp
-
-data class Photo(
-    val id: Int,
-    val resourceId: Int,
-    val title: String,
-    val category: String,
-    var isSelected: Boolean = false
-)
